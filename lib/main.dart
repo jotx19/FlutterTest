5882c827369c3ad99d04d71d3943724296bcef1f
Prabjot <prabjot24x7@gmail.com>
2024-05-14 20:10:06 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _counter = 0;
+  var myFontSize = 30.0;
+
+  void setNewValue(double value)
+  {
+    setState(() {
+      _counter = value.toInt();
+      myFontSize = value;
+    });
+  }
+
+  void _incrementCounter() {
+    setState(() {
+      if(_counter < 99.0)
+        _counter++;
+      myFontSize = _counter.toDouble();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            Text('You have pushed the button this many times:', style: TextStyle(fontSize: myFontSize, color: Colors.black), // Link to myFontSize
+            ),
+            Image.asset("images/algonquin.jpg", width: 200, height:200),
+            Text('$_counter', style: TextStyle(fontSize: myFontSize, color:Colors.red) ,),
+            Slider(value:_counter.toDouble(), max:100.0, onChanged: setNewValue, min:0.0 )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
