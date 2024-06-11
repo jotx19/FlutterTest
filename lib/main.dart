744fce6f0daf8fa93406f1618acdf5166f49c006
Prabjot <prabjot24x7@gmail.com>
2024-06-11 15:28:52 -0400

    week5

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        colorScheme: ColorScheme.light(),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Home Page'),
+      home: const MyHomePage(title: 'Flutter demo'),
     );
   }
 }
@@ -27,87 +26,158 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  var _counter = 0;
-  var myFontSize = 30.0;
-  var isChecked = false;
-  late TextEditingController _controller;
+class _LoginPageState extends State<MyHomePage> {
+  late TextEditingController _login;
+  late TextEditingController _passwords;
+  String imageSource = 'images/question-mark.png';
+
+  void _logining() {
+    setState(() {
+      if (_passwords.text == 'QWERTY123') {
+        imageSource = 'images/idea.png';
+      } else {
+        imageSource = 'images/stop.png';
+      }
+    });
+  }
+
+  void snackMan(String message) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      action: SnackBarAction(
+        label: 'Clear',
+        onPressed: () {
+          clearData();
+        },
+      ),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void window() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text('Save?'),
+        content: const Text('Save the credentials?'),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              saver();
+              Navigator.pop(context);
+            },
+            child: const Text('Yes'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              clearData();
+              Navigator.pop(context);
+            },
+            child: const Text('No'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void saver() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString("LoginName", _login.text);
+    await prefs.setString("Password", _passwords.text);
+  }
+
+  void loader() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? loginName = prefs.getString("LoginName");
+    String? password = prefs.getString("Password");
+
+    if (loginName != null && password != null) {
+      setState(() {
+        _login.text = loginName;
+        _passwords.text = password;
+      });
+      snackMan('Fields are loaded');
+    }
+  }
+
+  void clearData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove("LoginName");
+    await prefs.remove("Password");
+    setState(() {
+      _login.clear();
+      _passwords.clear();
+    });
+  }
 
   @override
   void initState() {
     super.initState();
-    _controller = TextEditingController();
+    _login = TextEditingController();
+    _passwords = TextEditingController();
+    loader();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _login.dispose();
+    _passwords.dispose();
     super.dispose();
-  }
-
-  void buttonClicked(){
-
-  }
-
-  void setNewValue(double value)
-  {
-    setState(() {
-      _counter = value.toInt();
-      myFontSize = value;
-    });
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      if(_counter < 99.0)
-        _counter++;
-      myFontSize = _counter.toDouble();
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-            Text('You have pushed the button this many times:', style: TextStyle(fontSize: myFontSize, color: Colors.black), // Link to myFontSize
-            ),
-            // Image.asset("images/algonquin.jpg", width: 200, height:200),
-            TextField(controller: _controller,
-                decoration: InputDecoration(
-                    hintText:"Type here",
-                    border: OutlineInputBorder(),
-                    labelText: "First name"
-                )),
-            Text('$_counter', style: TextStyle(fontSize: myFontSize, color:Colors.red) ,),
-            Slider(value:_counter.toDouble(), max:100.0, onChanged: setNewValue, min:0.0 ),
-            ElevatedButton(
-              onPressed: buttonClicked,
-               child: Image.asset("images/algonquin.jpg", width: 200, height:200)
+        title: const Text(
+          'Login page',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 38,
           ),
-            Checkbox(value: isChecked, onChanged:(newValue) { setState( () { isChecked = newValue !; } ); }),
-            Switch(value: isChecked, onChanged:(newValue) { setState( () { isChecked = newValue !; } ); })
-
+        ),
+        backgroundColor: Colors.amber,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _login,
+              decoration: InputDecoration(
+                labelText: 'Login',
+                labelStyle: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            TextField(
+              controller: _passwords,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                labelStyle: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                _logining();
+                window();
+              },
+              child: Text('Login'),
+            ),
+            SizedBox(height: 40),
+            Image.asset(
+              imageSource,
+              width: 300,
+              height: 300,
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
